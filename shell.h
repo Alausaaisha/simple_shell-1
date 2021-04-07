a79651c1f77a9cdd371e4b9b2c50b8f0fb903253
Erick Kiminza <kiminzaeric@gmail.com>
2021-04-07 12:09:32 +0300

    header file first commit

---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "shell.h"
+#endif
