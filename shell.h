6ac778f9258dfac6908ac44de207257b15dcd7b1
Erick Kiminza <kiminzaeric@gmail.com>
2021-04-12 10:48:41 +0300

    update header file

--- shell.h
+++ shell.h
@@ -1,10 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
+extern char **environ;
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "shell.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+void print_environ();
 #endif
